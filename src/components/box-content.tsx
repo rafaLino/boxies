79ee4eaa9c982a55bc08ae230e13269bbd26e1c3
Rafael Lino <rafael.linosp@gmail.com>
2025-06-17 02:47:19 -0300

    fix

--- src/components/box-content.tsx
+++ src/components/box-content.tsx
@@ -8,7 +8,7 @@ const areLargeTexts = (label: string, value: string) => {
 export const Content: FC<{ label: string, value?: string, color: string }> = memo(({ label, value = '', color }) => {
     const flexDirection = areLargeTexts(label, value) ? 'flex-col' : 'flex-row'
     return (
-        <div key={label} className={cn('flex gap-2 justify-between w-full', flexDirection)}>
+        <div key={label} className={cn('flex gap-2 justify-between w-full whitespace-pre-line', flexDirection)}>
             <span className={cn('capitalize font-bold', color)}>{label}</span>
             <CardDescription className={color}>{format(value)}</CardDescription>
         </div>
